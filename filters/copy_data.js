e5a51d5e3bfac6140468491e5a77c96830a9d373
Christian Kellermann <ckeen@pestilenz.org>
2010-10-29 17:25:11 +0200

    Adding parenthesis sometimes helps...

--- filters/copy_data.js
+++ filters/copy_data.js
@@ -1,5 +1,5 @@
 function(doc, req) {
-    if (doc.type && ( doc.type == "einnahme" || doc.type == "ausgabe" || doc.type == "config")){
+    if (doc.type && (( doc.type == "einnahme") || (doc.type == "ausgabe") || (doc.type == "config"))){
         return true;
     } else {
         return false;
